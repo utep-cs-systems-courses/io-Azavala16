32c2a521c8405ab93116c2c5b0a2f49eafd17778
Azavala16 <azavala16@miners.utep.edu>
2023-10-30 22:23:12 -0600

    separated tone 1 into its own function

--- project/11-buzzer-change-tone/buzzMain.c
+++ project/11-buzzer-change-tone/buzzMain.c
@@ -10,43 +10,47 @@ int main() {
 	or_sr(0x18);
 }
 
-void tone2_and3();
-
 int cycles, cyclesMin = 1500, cyclesMax = 4000;
-int tone1Interval = 0;
+int tone1Interval = 0, tone1Min = 100, tone1Max = 200;
+void tone1()
+{	
+	//quick change high and low pitch
+	if (tone1Interval <= tone1Min) cycles = cyclesMin; 
+	else if (tone1Interval  <= tone1Max) cycles = cyclesMax;	
+}
+
 int tone2Interval = 0, tone2Min = 1500, tone2Max = 3500;
-int tone3Interval = 0;
+int tone3Interval = 0, tone3Min = 5000, tone3Max = 7000;
+void tone2_and3()
+{
+	//tone2 intervals
+	if(tone2Interval >= tone2Min && tone2Interval <= tone2Max){
+		if(cycles <= cyclesMin) cycles = cyclesMax; //reset note to low pitch
+		else cycles -=6;  			    //gradual change low to high pitch
+	} 
+	//tone3 intervals (decrease to execute tone3 right after tone2)
+	else if (tone3Interval > tone3Min && tone3Interval <= tone3Max){ 
+		if(cycles >= cyclesMax) cycles = cyclesMin; //reset note to high pitch
+		else cycles +=12; 			    //gradual change high to low pitch
+	}
+	else tone1Interval =0;  //back to tone1 between tone2 and 3
+	
+}
+
 void
 __interrupt_vec(WDT_VECTOR) WDT()
 {
-
-	if (tone1Interval <= 100) cycles = cyclesMin; //tone 1
-	else if (tone1Interval  < 200) cycles = cyclesMax;
-	else tone2_and3();
-       	
-	tone1Interval++; 
+	tone1();
+	if(tone1Interval > tone1Max) tone2_and3();
 	
-	if (tone2Interval >= tone2Max){ 
-		if (tone3Interval >= 7000) {
-			tone3Interval = 0; tone2Interval =0; //reset tone2 and 3
-		} 		
+	tone1Interval++; //increase for tone2 to happen
+	
+	//if tone2 and 3 finished executing	
+	if (tone2Interval >= tone2Max && tone3Interval >= tone3Max){ 
+		tone3Interval = tone2Interval =0; //reset tone2 and 3
 	}
 	
-	tone2Interval++; tone3Interval++;
+	tone2Interval++; tone3Interval++; //increase for the other tones to happen
 	buzzer_set_period(cycles);
 }
 
-void tone2_and3()
-{
-	//change for 2nd part sooner
-	if(tone2Interval >= tone2Min && tone2Interval <= tone2Max){ 	//tone 2
-		if(cycles < cyclesMin) cycles = cyclesMax;
-		else cycles -=6;
-	}
-	else if (tone3Interval > 5000 && tone3Interval <= 7000){ // >3500 <= 6000 //tone 3
-		if(cycles >= 4000) cycles = 1500;
-		else cycles +=12;
-	}
-	else tone1Interval =0; //reset between cycles in tone 1
-	
-}
